6ccf3bf91b5c05f22614f0e37ac9a0852fbff06b
Marton Lederer <marton@lederer.hu>
2022-06-27 23:02:14 +0200

    chore: base

---
+++ src/pages/_app.tsx
@@ -0,0 +1,29 @@
+import type { AppProps } from "next/app";
+import styled, { ThemeProvider } from "styled-components";
+import "../styles/globals.css";
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={{
+      primary: "41, 7, 252"
+    }}>
+      <Gradient />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+const Gradient = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: radial-gradient(50% 50% at 50% 50%, rgba(${props => props.theme.primary}, 0.064) 0, rgba(255, 255, 255, 0) 100%);
+  z-index: -1;
+  width: 220vw;
+  height: 220vh;
+  transform: translate(-60vw, -110vh);
+`;
+
+export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Ark</title>
+        <link rel="icon" href="/icon.png" />
+      </Head>
+
+    </>
+  );
+}
+
+export default Home;
