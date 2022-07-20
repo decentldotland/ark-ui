9db58dddaa59db7d41d9426074f02c15c84e1f8c
xylophonez <xylophonezygote@gmail.com>
2022-07-20 08:09:42 +0200

    Color tweak, Read More button

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,8 @@ import "../styles/globals.css";
 function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={{
-      primary: "17, 157, 121",
+    //  primary: "17, 157, 121",
+      primary: "38, 191, 168",
       secondaryText: "#d3d3d3",
       tertiaryText: "#a3a3a3"
     }}>
