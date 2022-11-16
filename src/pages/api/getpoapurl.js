359bb08d282e7afae660de0bb6d5180396b9bb76
Max Knivets <knivets.m@gmail.com>
2022-11-16 16:30:52 -0500

    fix: splits up linking into two

--- src/pages/api/getpoapurl.js
+++ src/pages/api/getpoapurl.js
@@ -3,7 +3,6 @@ import { POAPS } from '../../utils/constants'
 export default async function handler(req, res) {
   try {
     const parsed = JSON.parse(POAPS)
-    console.log(parsed[req.body.arweave_address])
     res.status(200).json(parsed[req.body.arweave_address])
   } catch (error) {
     console.error(error)
