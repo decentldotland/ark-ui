a2f254e17af315d80cd88438c4d494d13e48d7de
Max Knivets <knivets.m@gmail.com>
2022-12-28 03:21:29 -0500

    feat: header nav are now proper buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,19 @@
 import Link from "next/link";
+import Button from "./Button";
+import { useRouter } from "next/router";
 
-const buttonStyle = 'hover:text-gray-300 cursor-pointer';
+const Footer = () => {
+  const router = useRouter();
 
-const Footer = () => (
-  <div className="fixed w-full bg-black/75 backdrop-blur-md z-[500] h-14 text-white flex text-center items-center flex-row gap-x-4 pl-4">
-    <Link href="/">
-      <button className={buttonStyle}>Ark Protocol</button>
-    </Link>
-    <Link href="/connections">
-      <button className={buttonStyle}>My Ark ID</button>
-    </Link>
-  </div>
-);
-
+  return(
+    <div className="fixed w-full bg-black/75 backdrop-blur-md z-[500] h-14 text-white flex text-center items-center flex-row gap-x-4 pl-4">
+      <Link href="/">
+        <Button disabled={router.pathname === '/'}>Ark Protocol</Button>
+      </Link>
+      <Link href="/connections">
+        <Button disabled={router.pathname === '/connections'}>My Ark ID</Button>
+      </Link>
+    </div>
+  );
+}
 export default Footer;
